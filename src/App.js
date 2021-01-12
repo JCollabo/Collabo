afd832f5286e50f8579713f76c8ce185030a3625
justinkretschman <73443789+justinkretschman@users.noreply.github.com>
2021-01-12 14:47:31 -0500

    boom

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { Helmet } from 'react-helmet';
 
-import collaboC from './CollaboC.png'
+import collaboC from './CollaboC.png';
 
 /*import { BrowserRouter as Router } */
 
-from firebase.initializeApp ({
+firebase.initializeApp({
   apiKey: "AIzaSyCt0AapeDmduiTedkzN7DFrkKWL6yUTBdg",
     authDomain: "collabo-chat.firebaseapp.com",
     databaseURL: "https://collabo-chat-default-rtdb.firebaseio.com",
