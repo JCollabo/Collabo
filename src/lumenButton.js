17e6178ecddd70267553a017df9f1e85599b4c2f
justinkretschman <73443789+justinkretschman@users.noreply.github.com>
2021-01-30 16:11:20 -0500

    lightbulb with 0, no addLumen

--- src/lumenButton.js
+++ src/lumenButton.js
@@ -7,32 +7,15 @@ class LumenButton extends React.Component {
   
   render() {
       
-        return <button> 💡 {this.state.lumens} onClick{addLumen} </button>
+        return <button> 💡 {this.state.lumens} </button>
     }
   }
   
-  addLumen = () => {
+  /*addLumen = () => {
       let newCount = this.state.lumens + 1;
         this.setState({
         lumens: newCount
       });
-    };
-
-/*class LumenButton extends React.Component {
-    state = {
-      lumens: 0
-    };
-    render(){
-      return <button> Lumens: {this.state.lumens} </button>
-    }
-  }
-
-  addLumen = () => {
-    let lumenValue = this.state.lumens + 1;
-      this.setState({
-      lumens: lumenValue
-      });
-  };*/
-
+    };*/
 
   export default LumenButton;
