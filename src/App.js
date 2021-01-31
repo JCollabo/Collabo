1314c83951bd4a9a13ab022e513890cd6947ba69
justinkretschman <73443789+justinkretschman@users.noreply.github.com>
2021-01-31 14:24:27 -0500

    dump old counter

--- src/App.js
+++ src/App.js
@@ -120,17 +120,22 @@ function ChatRoom() {
   </>)
 }
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter />
+      </div>
+    );
+  }
+}
+
 class Counter extends Component {
   state = {
     count: 0
   };
-  handleClick = () => {
-    this.setState(({ count }) => ({
-      count: count + 1
-    }));
-  };
   render() {
-    return <button onClick={this.handleClick}>{this.state.count}</button>;
+    return <button onClick={alert('Click me...')}>{this.state.count}</button>;
   }
 }
 
