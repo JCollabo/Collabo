c1aa9bc4632432bfbc6b141926db1e670553b436
justinkretschman <73443789+justinkretschman@users.noreply.github.com>
2021-02-02 20:38:12 -0500

    make work

---
+++ build/precache-manifest.5b79e98bde78968c645de643c7744136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233916663ce1d786e99d9092a02c2c54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d165ff8a68549462275b",
+    "url": "/static/css/main.1f3bbc41.chunk.css"
+  },
+  {
+    "revision": "54af7713c893eacbb7ca",
+    "url": "/static/js/2.3e08fad4.chunk.js"
+  },
+  {
+    "revision": "7d8dbc766008266d801fb16548a87b19",
+    "url": "/static/js/2.3e08fad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d165ff8a68549462275b",
+    "url": "/static/js/main.c5685cbb.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
